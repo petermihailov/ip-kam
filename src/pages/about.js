f0ccaacacf00b188c40bd58d633d588c8fdbd5d8
Peter Mihailov <usernameispeter@gmail.com>
2018-07-06 12:17:14 +0300

    add about styles

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,11 +1,10 @@
 import React, { Fragment } from 'react'
 import { withStyles } from '@material-ui/core/styles'
 import Helmet from 'react-helmet'
-import Paper from '@material-ui/core/Paper'
-import Grid from '@material-ui/core/Grid'
 import Typography from '@material-ui/core/Typography'
 import HeaderBanner from '../components/header-banner/header-banner'
 import MapSection from '../sections/index/map-section/map-section'
+import Container from '../components/container/container'
 
 const s = theme => ({
   container: {
@@ -26,37 +25,33 @@ const AboutPage = ({ classes }) => (
         { name: 'keywords', content: 'видеонаблюдение, о компании, ip-kam' },
       ]}
     />
-    <HeaderBanner title="О нас" />
-    <Paper elevation={0} square className={classes.container}>
-      <Grid container spacing={16}>
-        <Grid item xs={12}>
-          <Typography paragraph>
-            Наша команда работает с 2012 года. Мы занимаемся установкой
-            современных IP-систем видеонаблюдения, реализующих охранные функции
-            и возможности самостоятельного контроля своего бизнеса или частной
-            собственности за разумные деньги.
-          </Typography>
-          <Typography paragraph>
-            За 5 лет работы мы установили и поддерживаем более 450 систем
-            безопасности в Москве и Московской области
-          </Typography>
-          <Typography paragraph>
-            Каждый проект проходит 5 стадий контроля качества, сопровождается
-            личным менеджером, а все расчёты проводятся персональным инженером
-            проекта
-          </Typography>
-          <Typography>
-            Мы профессионально устанавливаем аналоговые системы видеонаблюдения.
-            Наша деятельность лицензирована, гарантируется 100%-ная прозрачность
-            сотрудничества. Предоставляется официальная гарантия, оказывается
-            постгарантийная техническая поддержка на всём протяжении
-            эксплуатации видеотехники. В цену комплекта оборудования входит
-            стоимость работ и расходных материалов.
-          </Typography>
-        </Grid>
-      </Grid>
-    </Paper>
-    <MapSection />
+    <HeaderBanner title="О нас"/>
+    <Container>
+      <Typography paragraph>
+        Наша команда работает с 2012 года. Мы занимаемся установкой
+        современных IP-систем видеонаблюдения, реализующих охранные функции
+        и возможности самостоятельного контроля своего бизнеса или частной
+        собственности за разумные деньги.
+      </Typography>
+      <Typography paragraph>
+        За 5 лет работы мы установили и поддерживаем более 450 систем
+        безопасности в Москве и Московской области
+      </Typography>
+      <Typography paragraph>
+        Каждый проект проходит 5 стадий контроля качества, сопровождается
+        личным менеджером, а все расчёты проводятся персональным инженером
+        проекта
+      </Typography>
+      <Typography>
+        Мы профессионально устанавливаем аналоговые системы видеонаблюдения.
+        Наша деятельность лицензирована, гарантируется 100%-ная прозрачность
+        сотрудничества. Предоставляется официальная гарантия, оказывается
+        постгарантийная техническая поддержка на всём протяжении
+        эксплуатации видеотехники. В цену комплекта оборудования входит
+        стоимость работ и расходных материалов.
+      </Typography>
+    </Container>
+    <MapSection/>
   </Fragment>
 )
 
